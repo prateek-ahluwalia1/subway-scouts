1527af559ec42b418816edff779e2337077013d8
Prateek <96107106+prateek-ahluwalia1@users.noreply.github.com>
2026-07-06 10:39:07 +0500

    jobcontroller comment portal notifications

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -457,17 +457,17 @@ if (empty($green_call_30)) {
         $admins = DB::table('users')->where('status', 'active')->get();
         // if(count($admins) > 0){
         //     foreach ($admins as $key => $value) {
-                $notification = array(
-                    'roster' => !empty($main_roster->id) ? $main_roster->id : null,
-                    'guard_id' => $this->currentUser->id, 
-                    'record_id' => $id,
-                    'message' =>  $guard->first_name.' '.$guard->last_name.' missed their 30 mints green call job at '. $job->site_name,
-                    'type' => 'greencall_missed',
-                    'send_time' => time(),
-                    'title' => 'Green Call Missed',
-                    // 'send_to' => $value->id,
-                );
-                DB::table('portal_notifications')->insert($notification);
+                // $notification = array(
+                //     'roster' => !empty($main_roster->id) ? $main_roster->id : null,
+                //     'guard_id' => $this->currentUser->id, 
+                //     'record_id' => $id,
+                //     'message' =>  $guard->first_name.' '.$guard->last_name.' missed their 30 mints green call job at '. $job->site_name,
+                //     'type' => 'greencall_missed',
+                //     'send_time' => time(),
+                //     'title' => 'Green Call Missed',
+                //     // 'send_to' => $value->id,
+                // );
+                // DB::table('portal_notifications')->insert($notification);
         //     }
         // }
 
