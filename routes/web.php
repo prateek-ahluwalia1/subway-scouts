46c1b58c2729502de54351fddb988749c24a10aa
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-07-07 11:29:12 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@
 
 $router->group(['prefix' => 'api/v1/'], function () use ($router) {
 
+    //subway app apis
+    $router->get('get-today-shifts/{site_id}', 'Api\JobController@getTodayShifts');
+    $router->get('get-branches', 'Api\JobController@getBranches');
+
+    
 
     $router->post('/guest_login', 'Api\JobController@guestSignin');
     $router->post('/guest_logout/{id}', 'Api\JobController@guestSignout');
@@ -69,8 +74,6 @@ $router->group(['prefix' => 'api/v1/'], function () use ($router) {
 
         $router->group(['prefix' => 'job'], function () use ($router) {
             $router->post('scan-QRCode/{id}', 'Api\JobController@scanQR');
-            $router->get('get-today-shifts/{site_id}', 'Api\JobController@getTodayShifts');
-            $router->get('get-branches', 'Api\JobController@getBranches');
 
             $router->post('/guard_sos_call/{id}', 'Api\JobController@guard_sos_call');
             $router->post('/guard/jobs/{type}/{duration}', 'Api\JobController@getGuardJobs');
